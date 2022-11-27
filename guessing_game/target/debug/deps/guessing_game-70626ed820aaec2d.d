bc5d2da0f9a04bd86d7178a55e16d0d17f66cc08
viodid <david.yunta.aller@gmail.com>
2022-11-27 23:43:32 +0100

    guess number first part

---
+++ guessing_game/target/debug/deps/guessing_game-70626ed820aaec2d.d
@@ -0,0 +1,5 @@
+/home/viodid/Documents/Rust/the-book/guessing_game/target/debug/deps/guessing_game-70626ed820aaec2d.rmeta: src/main.rs
+
+/home/viodid/Documents/Rust/the-book/guessing_game/target/debug/deps/guessing_game-70626ed820aaec2d.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/viodid/Documents/Rust/the-book/guessing_game/target/debug/guessing_game: /home/viodid/Documents/Rust/the-book/guessing_game/src/main.rs
