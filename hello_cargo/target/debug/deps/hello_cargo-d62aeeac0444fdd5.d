bf5c2ead7617b513b13ab1878d63f1eef86a780f
David Yunta <david.yunta.aller@gmail.com>
2022-12-02 01:18:11 +0100

    Delete hello_cargo/target directory

--- hello_cargo/target/debug/deps/hello_cargo-d62aeeac0444fdd5.d
+++
@@ -1,5 +0,0 @@
-/home/viodid/Documents/Rust/the-book/hello_cargo/target/debug/deps/hello_cargo-d62aeeac0444fdd5.rmeta: src/main.rs
-
-/home/viodid/Documents/Rust/the-book/hello_cargo/target/debug/deps/hello_cargo-d62aeeac0444fdd5.d: src/main.rs
-
-src/main.rs:
