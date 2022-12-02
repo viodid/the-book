3dfa0e530f08490d4f6cf9e6668beb20a542f800
David Yunta <david.yunta.aller@gmail.com>
2022-12-02 01:17:44 +0100

    Delete guessing_game/target directory

--- guessing_game/target/debug/deps/guessing_game-2e7ae2c660a760a1.d
+++
@@ -1,5 +0,0 @@
-/home/viodid/Documents/Rust/the-book/guessing_game/target/debug/deps/guessing_game-2e7ae2c660a760a1.rmeta: src/main.rs
-
-/home/viodid/Documents/Rust/the-book/guessing_game/target/debug/deps/guessing_game-2e7ae2c660a760a1.d: src/main.rs
-
-src/main.rs:
--- guessing_game/target/debug/deps/guessing_game-ceabc76c55ac354c.d
+++
@@ -1,5 +0,0 @@
-/home/viodid/Documents/Rust/the-book/guessing_game/target/debug/deps/guessing_game-ceabc76c55ac354c.rmeta: src/main.rs
-
-/home/viodid/Documents/Rust/the-book/guessing_game/target/debug/deps/guessing_game-ceabc76c55ac354c.d: src/main.rs
-
-src/main.rs:
